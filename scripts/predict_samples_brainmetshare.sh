10b3813f2a4547d941e0ff5a08ddd527bc3bc724
jrtaloma <jrtaloma@gmail.com>
2022-04-11 00:49:38 +0200

    prediction supports segmentations

--- scripts/predict_samples_brainmetshare.sh
+++ scripts/predict_samples_brainmetshare.sh
@@ -1,2 +1,2 @@
 ### Using RGB images only. Predict with U-Net.
-python predict_samples.py --name brainmetshare_t1pre_t1post --gpu_ids 0 --dataroot ./datasets/stanford_release_brainmask/axial/t1pre_t1post --netG unet --label_nc 0 --class_labels 0 255 --resize_or_crop none --which_epoch latest
+python predict_samples.py --name brainmetshare_seg_t1pre_t1post --gpu_ids 0 --dataroot ./datasets/stanford_release_brainmask/axial/t1pre_t1post --netG unet --label_nc 0 --class_labels 0 255 --resize_or_crop none --which_epoch latest
